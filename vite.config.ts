92f5edce1e85f26e654f029076887991200fc810
Elliot Wright <elliot@Elliots-MacBook-Pro.local>
2024-04-27 13:02:28 +0100

    Worked around HMR issue with full reload instead

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig, Plugin } from "vite"
+import { svelte } from "@sveltejs/vite-plugin-svelte"
+
+const fullReloadAlways: Plugin = {
+  name: "full-reload",
+  handleHotUpdate({ server }) {
+    server.hot.send({ type: "full-reload" })
+    return []
+  },
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), fullReloadAlways],
 })
